3a0556e95b509ff4537ee8c8a9640b8fe33a97e2
pavankalyankumar <70573554+pavankalyankumar@users.noreply.github.com>
2020-11-25 10:18:44 +0530

    Update 02.py

--- 02.py
+++ 02.py
@@ -16,11 +16,19 @@ output: 0
 import unittest
 
 def min_row(m,n,l):
+    if h >= l: 
+        m = l + (h - l)//2
+        if (m == 0 or a[m - 1] == 0) and a[] == 1: 
+            return mid 
+        elif a[m] == 0: 
+            return first(a, (m + 1), h) 
+        else: 
+            return first(a, l, (m - 1)) 
+    return -1
     """
     m,n define size of matrix 
     l list of m*n elements
     """
-	pass
 
 # DO NOT TOUCH THE BELOW CODE
 class TestCommonWords(unittest.TestCase):
